908f9dc8f8ce32a460c55e6128e4f21da7910f05
Truman Tong <trumantong8@gmail.com>
2026-01-23 14:33:10 +0800

    Level-0

---
+++ src/main/java/JiaRui.java
@@ -0,0 +1,11 @@
+public class JiaRui {
+    public static void main(String[] args) {
+        String name = "____________________________________________________________\n"
+                + "Hello! I'm JiaRui\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(name);
+    }
+}
