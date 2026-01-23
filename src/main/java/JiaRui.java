5f329e47899d47335e82e67ef0c10c48833835df
Truman Tong <trumantong8@gmail.com>
2026-01-23 15:06:22 +0800

    Level 1

--- src/main/java/JiaRui.java
+++ src/main/java/JiaRui.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class JiaRui {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
         String name = "____________________________________________________________\n"
                 + "Hello! I'm JiaRui\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+
+
         System.out.println(name);
+        String line = in.nextLine();
+
+        while(!line.equals("bye")){
+            System.out.println("____________________________________________________________\n"
+                + line
+                + "\n____________________________________________________________\n");
+
+            line = in.nextLine();
+        }
+
+        System.out.println("____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n");
+
     }
 }
