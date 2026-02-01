0af62af71db5047afbcafe88ede41cff14373ba1
Truman Tong <trumantong8@gmail.com>
2026-02-01 12:16:18 +0800

    Add Jar

--- src/test/java/jiarui/EventTest.java
+++ src/test/java/jiarui/EventTest.java
@@ -1,4 +1,35 @@
 package jiarui;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EventTest {
+    @Test
+    public void event_toString_notDone_success() {
+        Event e = new Event(false, "project meeting", "Aug 6th 2pm", "4pm");
+        assertEquals("[E][ ] project meeting (from: Aug 6th 2pm to: 4pm)", e.toString());
+    }
+
+    @Test
+    public void event_toString_done_success() {
+        Event e = new Event(true, "project meeting", "Aug 6th 2pm", "4pm");
+        assertEquals("[E][X] project meeting (from: Aug 6th 2pm to: 4pm)", e.toString());
+    }
+
+    @Test
+    public void event_getters_success() {
+        Event e = new Event(false, "project meeting", "Aug 6th 2pm", "4pm");
+        assertEquals("Aug 6th 2pm", e.getStart());
+        assertEquals("4pm", e.getEnd());
+    }
+
+    @Test
+    public void event_markAsCompleted_updatesToString() {
+        Event e = new Event(false, "project meeting", "Aug 6th 2pm", "4pm");
+        e.markAsCompleted();
+        assertEquals("[E][X] project meeting (from: Aug 6th 2pm to: 4pm)", e.toString());
+    }
+
+
+
 }
--- src/test/java/jiarui/TodoTest.java
+++ src/test/java/jiarui/TodoTest.java
@@ -1,4 +1,25 @@
 package jiarui;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class TodoTest {
+    @Test
+    public void todo_toString_notDone_success() {
+        assertEquals("[T][ ] eat", new ToDo(false, "eat").toString());
+    }
+
+    @Test
+    public void todo_toString_done_success() {
+        assertEquals("[T][X] eat", new ToDo(true, "eat").toString());
+    }
+
+    @Test
+    public void todo_markDone_updatesToString() {
+        ToDo t = new ToDo(false, "eat");
+        t.markAsCompleted();
+        assertEquals("[T][X] eat", t.toString());
+    }
+
 }
